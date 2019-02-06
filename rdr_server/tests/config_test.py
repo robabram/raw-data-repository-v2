f064b1a1da862951eaf867d0de0917402af350d7
Robert Abram <robert.abram@entpack.com>
2019-02-06 15:02:39 -0700

    Added shebang to runner.py

--- rdr_server/tests/config_test.py
+++ rdr_server/tests/config_test.py
@@ -4,16 +4,7 @@
 #
 
 import unittest
-
-
-def multiply(a, b):
-    """
-    >>> multiply(4, 3)
-    12
-    >>> multiply('a', 3)
-    'aaa'
-    """
-    return a * b
+from rdr_server.config import config
 
 
 class TestUM(unittest.TestCase):
@@ -21,8 +12,5 @@ class TestUM(unittest.TestCase):
     def setUp(self):
         pass
 
-    def test_numbers_3_4(self):
-        self.assertEqual(multiply(3, 4), 12)
-
-    def test_strings_a_3(self):
-        self.assertEqual(multiply('a', 3), 'aaa')
+    def test_gcp_project(self):
+        self.assertEquals('all-of-us-rdr-local', config.GCP_PROJECT)
